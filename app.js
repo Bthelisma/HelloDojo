032b4f45c82f4a4275ce630f12d1dd14d77ecc7f
Bthelisma <thelismabeatrice@gmail.com>
2021-05-03 13:25:58 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const grettings = React.createElement("div", null, 
+	React.createElement("h1", null, "Hello Dojo!"),
+	React.createElement("h4", null, "Things I need to do:"),
+	React.createElement("p", null, "Learn React"),
+	React.createElement("p", null, "Climb the Mt Everest"),
+	React.createElement("p", null, "Feed the dogs"),
+);
+
+ReactDOM.render(myButton, document.getElementById('app'));
